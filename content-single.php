3b6a2576e59646560b69bff801c587dc8445e2f1
David Harms <davidharms@Davids-MacBook-Pro-2.local>
2016-10-11 16:47:25 -0400

    initial

---
+++ content-single.php
@@ -0,0 +1,4 @@
+<h2><a href='<?php the_permalink() ?>'><?php the_title() ?></a></h2>
+<div class="content">
+	<?php the_content() ?>
+</div>
---
+++ none.php
@@ -0,0 +1,4 @@
+<h2><a href='<?php the_permalink() ?>'><?php the_title() ?></a></h2>
+<div class="content">
+	  <p>Oh No, there are no posts!</p>
+</div>
